4ab0802b57ad1107162c863e83d5aae99e2223e9
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-16 00:28:39 +0200

    add generate numbers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
 addEventListener("DOMContentLoaded", () => {
-    const grid = document.querySelector('.grid');
+    const gridDisplay = document.querySelector('.grid');
     const scoreDisplay = document.querySelector("score")
+    const result = document.querySelector("result")
+    const width = 4;
+    let squares = []
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            square = document.createElement('div');
+            square.innerHTML = 0;
+            gridDisplay.appendChild(square);
+            squares.push(square)
+        }
+        generateRandomNumber();
+    }
+    createBoard();
+
+    function generateRandomNumber() {
+        randomNumber = Math.floor(Math.random() * squares.length)
+        if(squares[randomNumber] == 0){
+            squares[randomNumber].innerHTML = 2;
+        }else {
+            generateRandomNumber();
+        }
+    }
+
 })
