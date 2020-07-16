334cad5499c39fe329830d7cca24234fa6131da0
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-17 00:37:08 +0200

    Add swipe down and up

--- js/app.js
+++ js/app.js
@@ -69,8 +69,62 @@ addEventListener("DOMContentLoaded", () => {
             }
         }
     }
+    
+    function moveDown(){
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+(width)].innerHTML
+            let totalThree = squares[i+(width*2)].innerHTML
+            let totalFour = squares[i+(width*3)].innerHTML
+            let columns = [parseInt(totalOne),parseInt(totalTwo ),parseInt(totalThree),parseInt(totalFour)]
+            let filteredColumns = columns.filter(num => num)
+            let missingSquares = 4 - filteredColumns.length;
+            let zeros = Array(missingSquares).fill(0);
+            let newColumn = [ ...zeros,...filteredColumns]
+            squares[i].innerHTML = newRow[0]
+            squares[i + (width)].innerHTML = newColumn[1]
+            squares[i + (width*2)].innerHTML = newColumn[2]
+            squares[i + (width*3)].innerHTML = newColumn[3]
+        }
+    }
+
+    function moveUp(){
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+(width)].innerHTML
+            let totalThree = squares[i+(width*2)].innerHTML
+            let totalFour = squares[i+(width*3)].innerHTML
+            let columns = [parseInt(totalOne),parseInt(totalFour),parseInt(totalThree),parseInt(totalFour)]
+            let filteredColumns = columns.filter(num => num)
+            let missingSquares = 4 - filteredColumns.length;
+            let zeros = Array(missingSquares).fill(0);
+            let newColumn = [ ...filteredColumns,...zeros]
+            squares[i].innerHTML = newRow[0]
+            squares[i + (width)].innerHTML = newColumn[1]
+            squares[i + (width*2)].innerHTML = newColumn[2]
+            squares[i + (width*3)].innerHTML = newColumn[3]
+        }
+    }
 
     function combineRow() {
+        for (let i = 0; i < 15; i++) {
+            if (squares[i].innerHTML === squares[i + 1].innerHTML) {
+                let totalTwoSquares = parseInt(squares[i].innerHTML) + parseInt(squares[i].innerHTML)
+                squares[i].innerHTML = totalTwoSquares;
+                squares[i + 1].innerHTML = 0
+            }
+        }
+    }
+    function combineRow() {
+        for (let i = 0; i < 15; i++) {
+            if (squares[i].innerHTML === squares[i + 1].innerHTML) {
+                let totalTwoSquares = parseInt(squares[i].innerHTML) + parseInt(squares[i].innerHTML)
+                squares[i].innerHTML = totalTwoSquares;
+                squares[i + 1].innerHTML = 0
+            }
+        }
+    }
+    function combineColumn() {
         for (let i = 0; i < 15; i++) {
             if (squares[i].innerHTML === squares[i + 1].innerHTML) {
                 let totalTwoSquares = parseInt(squares[i].innerHTML) + parseInt(squares[i].innerHTML)
