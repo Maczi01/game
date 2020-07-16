54267d051d46331858e57ce1f650d4ddf85a847f
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-16 22:02:20 +0200

    swipe function - add new row

--- js/app.js
+++ js/app.js
@@ -26,8 +26,18 @@ addEventListener("DOMContentLoaded", () => {
 
     function moveRight(){
         for (let i = 0; i < 16; i++) {
-
+            if(i%4 === 0){
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row =[parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                console.log(row)
+                let filteredRow = row.filter(num => num)
+                console.log(filteredRow)
+            }
         }
     }
+    moveRight()
 
 })
