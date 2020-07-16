cfbdb2767aee21f28f5ce193ec0f7c05a3ac6659
Maczi01 <mateusz.w.twardy@gmail.com>
2020-07-16 23:30:11 +0200

    add move left

--- js/app.js
+++ js/app.js
@@ -40,9 +40,45 @@ addEventListener("DOMContentLoaded", () => {
                 let zeros = Array(missingSquares).fill(0);
                 console.log(zeros)
                 console.log("===")
+                let newRow = [...zeros,...filteredRow]
+                console.log({newRow})
+
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
             }
         }
     }
-    moveRight()
+
+    function moveLeft(){
+        for (let i = 0; i < 16; i++) {
+            if(i%4 === 0){
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row =[parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                console.log(row)
+                let filteredRow = row.filter(num => num)
+                console.log(filteredRow)
+                let missingSquares = 4 - filteredRow.length;
+                let zeros = Array(missingSquares).fill(0);
+                console.log(zeros)
+                console.log("===")
+                let newRow = [...filteredRow,...zeros]
+                console.log({newRow})
+
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+    moveLeft()
 
 })
